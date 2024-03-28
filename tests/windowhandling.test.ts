b63cc2e3110da65aa899c6c4dc897dd9dea92125
Kavippiriyan <kavippiriyan2001@gmail.com>
2024-03-28 17:21:27 +0530

    windowhandling 28/03/2024

--- tests/windowhandling.test.ts
+++ tests/windowhandling.test.ts
@@ -1,18 +1,44 @@
 import { test } from "@playwright/test"
 
-test("window_handle", async ({ page }) => {
-    await page.goto("https://www.globalsqa.com/demo-site/frames-and-windows/#Open%20New%20Tab")
+// test("window_handle", async ({ page }) => {
+//     await page.goto("https://www.globalsqa.com/demo-site/frames-and-windows/#Open%20New%20Tab")
+
+//     console.log(page.url());
+
+
+//     const [newtab] = await Promise.all(
+//         [
+//             page.waitForEvent("popup"), page.click("'Click Here'")
+//         ]
+//     )
+
+//     console.log(newtab.url());
+//     newtab.click("[id='Open New Window']")
+//     await page.waitForTimeout(2000);
+// })
+
+test("multiple window_handle", async ({ page }) => {
+    await page.goto("https://www.hyrtutorials.com/p/window-handles-practice.html")
 
     console.log(page.url());
 
 
-    const [newtab] = await Promise.all(
+    const [multiple_windows] = await Promise.all(
         [
-            page.waitForEvent("popup"), page.click("'Click Here'")
+            page.waitForEvent("popup"),
+            page.click("'Open Multiple Windows'"),
+            page.click("'Open Multiple Windows'")
         ]
     )
+    console.log(multiple_windows.url());
+    const no_of_windows = multiple_windows.context().pages();
+    console.log("no_of_windows: ", no_of_windows);
 
-    console.log(newtab.url());
-    newtab.click("[id='Open New Window']")
-    await page.waitForTimeout(2000);
+    await page.waitForLoadState();
+
+    no_of_windows.forEach(windows => {
+        console.log(windows.url());
+
+    })
+
 })
