e50186a0ed3a0593520108c65630aad11459d66b
Kavippiriyan <kavippiriyan2001@gmail.com>
2024-03-30 12:21:25 +0530

    download and upload files 30/03/2024

--- tests/upload_download.test.ts
+++ tests/upload_download.test.ts
@@ -1,22 +1,47 @@
 
 import { test } from "@playwright/test"
 
-test("download files", async ({ page }) => {
-    await page.goto("https://demo.automationtesting.in/FileDownload.html")
+// test("download files", async ({ page }) => {
+//     await page.goto("https://demo.automationtesting.in/FileDownload.html")
 
-    await page.type("//textarea[@id='textbox']", "Hello My Family")
-    await page.waitForTimeout(2000);
-    await page.click("(//button[text()='Generate File'])[1]");
+//     await page.type("//textarea[@id='textbox']", "Hello My Family")
+//     await page.waitForTimeout(2000);
+//     await page.click("(//button[text()='Generate File'])[1]");
 
-    const download = await Promise.all(
+//     const download = await Promise.all(
+//         [
+//             page.waitForEvent("download"),
+//             page.click('//a[@id="link-to-download"]')
+//         ]
+//     )
+
+//     // const path = await download[0].path();
+//     // console.log(path);
+
+//     const path = await download[0].suggestedFilename()
+
+//     await download[0].saveAs(path);
+
+// })
+
+
+test("upload files", async ({ page }) => {
+    await page.goto("https://blueimp.github.io/jQuery-File-Upload/");
+
+    // await page.setInputFiles("input[type='file']", ["images_for_playwright/Vishnu god.png", "images_for_playwright/Krishna.png"])
+
+    let [uploadfiles] = await Promise.all(
         [
-            page.waitForEvent("download"),
-            page.click('//a[@id="link-to-download"]')
+            page.waitForEvent("filechooser"),
+            page.click("input[type='file']")
         ]
     )
+    let ismultiple = uploadfiles.isMultiple();
+    console.log(ismultiple);
 
-    const path = await download[0].path();
-    console.log(path);
-    
+    uploadfiles.setFiles(["images_for_playwright/Vishnu god.png", "images_for_playwright/Krishna.png"])
+
+    await page.waitForTimeout(2000);
+
 
 })
