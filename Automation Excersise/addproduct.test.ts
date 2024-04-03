c94256dfc317713d1acc82de4e9ff89adf926d6c
Kavippiriyan <kavippiriyan2001@gmail.com>
2024-04-03 18:30:35 +0530

    04/03/2024 Add to Cart

--- Automation Excersise/addproduct.test.ts
+++ Automation Excersise/addproduct.test.ts
@@ -22,22 +22,65 @@ export default class AddCart {
 
     }
     async validatingthecarts() {
+        let dress1 = await this.page.locator("(//p[text()='Rs. 500'])[1]")
+        let dress1_t = await this.page.locator("(//p[text()='Rs. 500'])[2]")
+        let dress2 = await this.page.locator("(//p[text()='Rs. 400'])[1]")
+        let dress2_t = await this.page.locator("(//p[text()='Rs. 400'])[2]")
+        let dress1_quan = await this.page.locator("(//button[text()='1'])[1]")
+        let dress2_quan = await this.page.locator("(//button[text()='1'])[2]")
 
-        let text = 'Rs. 500'
-        for (let i = 0; i < 2; i++) {
-            let paracontent = await this.page.textContent(`(//p[text()='${text}'])[${i}]`)
-            expect(paracontent).toEqual(text)
+        let dress1_price = await dress1.innerText()
+        let trimdress1 = dress1_price.substring(4).trim();
+        let convertdress1 = Number(trimdress1)
+        console.log(convertdress1);
+
+        let dress2_price = await dress2.innerText()
+        let trimdress2 = dress2_price.substring(4).trim();
+        let convertdress2 = Number(trimdress2)
+        console.log(convertdress2);
+
+        let dress1_quantity = await dress1_quan.innerText()
+        let trimdress1_quantity = dress1_quantity.trim();
+        let convertdress1_quantity = Number(trimdress1_quantity)
+        console.log(convertdress1_quantity);
+
+        let dress2_quantity = await dress2_quan.innerText()
+        let trimdress2_quantity = dress2_quantity.trim();
+        let convertdress2_quantity = Number(trimdress2_quantity)
+        console.log(convertdress2_quantity);
+
+        let dress1_total = await dress1_t.innerText()
+        let trimdress1_total = dress1_total.substring(4).trim();
+        let convertdress1_total = Number(trimdress1_total)
+        console.log(convertdress1_total);
+
+        let dress2_total = await dress2_t.innerText()
+        let trimdress2_total = dress2_total.substring(4).trim();
+        let convertdress2_total = Number(trimdress2_total)
+        console.log(convertdress2_total);
+
+        if ((convertdress1 * convertdress1_quantity) == convertdress1_total) {
+            console.log("dress1 verified");
+
         }
-        let text1 = 'Rs. 400'
-        for (let i = 0; i < 2; i++) {
-            let paracontent1 = await this.page.textContent(`(//p[text()='${text}'])[${i}]`)
-            expect(paracontent1).toEqual(text1)
+        else {
+            console.log("dress1 not verified");
+
         }
-        let text2 = '1'
-        for (let i = 0; i < 2; i++) {
-            let paracontent2 = await this.page.textContent(`(//p[text()='${text}'])[${i}]`)
-            expect(paracontent2).toEqual(text2)
+        if ((convertdress2 * convertdress2_quantity) == convertdress2_total) {
+            console.log("dress2 verified");
+
         }
+        else {
+            console.log("dress1 not verified");
+
+        }
+
+        // console.log((convertdress1));
+        // console.log((convertdress1_quantity));
+        // console.log((convertdress1_total));
+
+
     }
 
 }
